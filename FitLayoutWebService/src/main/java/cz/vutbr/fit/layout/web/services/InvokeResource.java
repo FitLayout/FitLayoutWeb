5e909f446736d6b5a4cba8503243816fc317fcb3
Radek Burget <burgetr@fit.vutbr.cz>
2020-09-06 23:02:22 +0200

    Artifact provider invocation service

--- FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/services/InvokeResource.java
+++ FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/services/InvokeResource.java
@@ -1,6 +1,8 @@
 
 package cz.vutbr.fit.layout.web.services;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
@@ -8,18 +10,26 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.StreamingOutput;
+
+import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
 
 import cz.vutbr.fit.layout.api.ArtifactService;
 import cz.vutbr.fit.layout.api.ParametrizedOperation;
 import cz.vutbr.fit.layout.api.ServiceManager;
 import cz.vutbr.fit.layout.cssbox.CSSBoxTreeProvider;
 import cz.vutbr.fit.layout.model.Artifact;
+import cz.vutbr.fit.layout.model.Page;
+import cz.vutbr.fit.layout.rdf.BoxModelBuilder;
+import cz.vutbr.fit.layout.rdf.RESOURCE;
+import cz.vutbr.fit.layout.rdf.Serialization;
 
 /**
  *
@@ -75,7 +85,19 @@ public class InvokeResource
         {
             sm.setServiceParams(op, params.getParams());
             Artifact page = ((ArtifactService) op).process(null);
-            return Response.ok(page.getId()).build();
+            
+            IRI pageIri = RESOURCE.createPageURI(page.getId());
+            BoxModelBuilder builder = new BoxModelBuilder((Page) page, pageIri);
+            Model graph = builder.getGraph();
+            
+            StreamingOutput stream = new StreamingOutput() {
+                @Override
+                public void write(OutputStream os) throws IOException, WebApplicationException {
+                    Serialization.modelToJsonLDStream(graph, os);
+                }
+            };
+            
+            return Response.ok(stream).build();
         }
         else
         {
