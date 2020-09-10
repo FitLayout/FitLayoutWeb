b8155670ffedd422a1c460523403bbcf7f58c01e
Radek Burget <burgetr@fit.vutbr.cz>
2020-09-10 13:57:01 +0200

    Artifact creation: preparation

--- FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/services/ArtifactResource.java
+++ FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/services/ArtifactResource.java
@@ -5,15 +5,34 @@
  */
 package cz.vutbr.fit.layout.web.services;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Model;
+
+import cz.vutbr.fit.layout.api.ArtifactService;
+import cz.vutbr.fit.layout.api.ParametrizedOperation;
+import cz.vutbr.fit.layout.api.ServiceManager;
+import cz.vutbr.fit.layout.cssbox.CSSBoxTreeProvider;
+import cz.vutbr.fit.layout.model.Artifact;
+import cz.vutbr.fit.layout.model.Page;
+import cz.vutbr.fit.layout.rdf.BoxModelBuilder;
+import cz.vutbr.fit.layout.rdf.RESOURCE;
+import cz.vutbr.fit.layout.rdf.Serialization;
 
 /**
  * 
@@ -22,6 +41,18 @@ import org.eclipse.rdf4j.model.IRI;
 @Path("artifact")
 public class ArtifactResource extends BaseStorageResource
 {
+    private ServiceManager sm;
+    
+    
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("INIT");
+        sm = ServiceManager.create();
+        CSSBoxTreeProvider provider = new CSSBoxTreeProvider();
+        sm.addArtifactService(provider);
+        System.out.println("Services: " + sm.findArtifactSevices().keySet());
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -30,5 +61,37 @@ public class ArtifactResource extends BaseStorageResource
         Set<IRI> list = getStorage().getArtifactIRIs();
         return Response.ok(list).build();
     }
+    
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(ServiceParams params)
+    {
+        ParametrizedOperation op = sm.findParmetrizedService(params.getServiceId());
+        if (op != null)
+        {
+            sm.setServiceParams(op, params.getParams());
+            Artifact page = ((ArtifactService) op).process(null);
+            
+            IRI pageIri = RESOURCE.createPageURI(page.getId());
+            BoxModelBuilder builder = new BoxModelBuilder((Page) page, pageIri);
+            Model graph = builder.getGraph();
+            
+            StreamingOutput stream = new StreamingOutput() {
+                @Override
+                public void write(OutputStream os) throws IOException, WebApplicationException {
+                    Serialization.modelToJsonLDStream(graph, os);
+                }
+            };
+            
+            return Response.ok(stream).build();
+        }
+        else
+        {
+            return Response.status(Status.NOT_FOUND).entity("{\"error\": \"No such service\"}").build();
+        }
+    }
+
     
 }
