7692d65d45997f5a34f9f0c24a16e8eed2c0ab76
Radek Burget <burgetr@fit.vutbr.cz>
2021-02-18 20:25:20 +0100

    Docker scripts moved to separate repository

--- FitLayoutWebService/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && mvn payara-micro:bundle && docker build -t fitlayout/fitlayout-server .
-docker rm -f fitlayout-server || true && docker run -d -p 8400:8400 --name fitlayout-server --restart unless-stopped fitlayout/fitlayout-server 
