adc5f0d4d945719dbccd243a55bd9133fb91e280
Radek Burget <burgetr@fit.vutbr.cz>
2022-01-04 18:28:55 +0100

    Tag listing endpoint added

---
+++ FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/data/TagInfo.java
@@ -0,0 +1,64 @@
+/**
+ * TagInfo.java
+ *
+ * Created on 4. 1. 2022, 18:17:23 by burgetr
+ */
+package cz.vutbr.fit.layout.web.data;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import cz.vutbr.fit.layout.rdf.model.RDFTag;
+
+/**
+ * 
+ * @author burgetr
+ */
+@Schema(name="TagInfo", description="Tag information")
+public class TagInfo
+{
+    private String iri;
+    private String type;
+    private String name;
+    
+    public TagInfo()
+    {
+    }
+    
+    public TagInfo(RDFTag src)
+    {
+        this.iri = String.valueOf(src.getIri());
+        this.type = src.getType();
+        this.name = src.getValue();
+    }
+
+    public String getIri()
+    {
+        return iri;
+    }
+
+    public void setIri(String iri)
+    {
+        this.iri = iri;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
---
+++ FitLayoutWebService/src/main/java/cz/vutbr/fit/layout/web/services/TagsResource.java
@@ -0,0 +1,80 @@
+/**
+ * TagsResource.java
+ *
+ * Created on 4. 1. 2022, 18:23:25 by burgetr
+ */
+package cz.vutbr.fit.layout.web.services;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import cz.vutbr.fit.layout.rdf.RDFArtifactRepository;
+import cz.vutbr.fit.layout.rdf.model.RDFTag;
+import cz.vutbr.fit.layout.web.data.ResultErrorMessage;
+import cz.vutbr.fit.layout.web.data.TagInfo;
+import cz.vutbr.fit.layout.web.ejb.StorageService;
+import cz.vutbr.fit.layout.web.ejb.UserService;
+
+/**
+ * 
+ * @author burgetr
+ */
+@Path("r/{repoId}/tags")
+@Tag(name = "tags", description = "Tag operations")
+public class TagsResource
+{
+    @Inject
+    private UserService userService;
+    @Inject
+    private StorageService storage;
+
+    @PathParam("repoId")
+    @Parameter(description = "The ID of the artifact repository to use", required = true)
+    private String repoId;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @PermitAll
+    @Operation(operationId = "getTags", summary = "Reads a list of tags defined in the repository")
+    @APIResponse(responseCode = "200", description = "The list of tags",
+            content = @Content(schema = @Schema(ref = "TagInfo")))    
+    @APIResponse(responseCode = "404", description = "Repository with the given ID not found",
+        content = @Content(schema = @Schema(ref = "ResultErrorMessage")))    
+    public Response getTags()
+    {
+        final RDFArtifactRepository repo = storage.getArtifactRepository(userService.getUser(), repoId);
+        if (repo != null)
+        {
+            Collection<cz.vutbr.fit.layout.model.Tag> rdfTags = repo.getTags();
+            List<TagInfo> tags = rdfTags.stream().map(t -> new TagInfo((RDFTag) t)).collect(Collectors.toList());
+            return Response.ok(tags).build();
+        }
+        else
+        {
+            return Response.status(Status.NOT_FOUND)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity(new ResultErrorMessage(ResultErrorMessage.E_NO_REPO))
+                    .build();
+        }
+    }
+
+}
